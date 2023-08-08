aff1d54cd877bbf02331b1e888c215b57d3db73c
Ayam <ayamullahkhan04@gmail.com>
2023-08-08 23:37:42 +0530

    2nd

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # USAGE : python main.py
 from keras.models import load_model
 from time import sleep
-from tensorflow.keras.utils import img_to_array
+from tensorflow.python.keras.utils import img_to_array
 from keras.preprocessing import image
 import cv2
 import numpy as np
